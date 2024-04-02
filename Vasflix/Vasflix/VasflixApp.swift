0688bff0ebfe19d8ccdfef8e72a46dc3de21c7cb
Adrian Castañeda Rodriguez <ing.adrian.castaneda@gmail.com>
2024-04-02 12:53:19 -0700

    project setup

---
+++ Vasflix/Vasflix/VasflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  VasflixApp.swift
+//  Vasflix
+//
+//  Created by Adrian Castañeda on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct VasflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
